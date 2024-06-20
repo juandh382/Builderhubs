963c2a784b3a2f8cc7a936a5be9e8231b234b002
juandh382 <jhern886@gmail.com>
2024-06-20 00:47:28 -0400

    thirth attempt deploying

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/Builderhubs",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    
-};
+const nextConfig = {};
 
 export default nextConfig;
